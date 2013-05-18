42b6a19ea4327665df4489b7437a49adc6ee3f5a
Zack <zachmokahn@gmail.com>
2013-05-17 20:56:31 -0500

    Update p4.rb
    Not that it matters at all, I'm sure you're just doing it to quiz your knowledge and learn like me.

--- p4.rb
+++ p4.rb
@@ -1,22 +1,32 @@
-#!/usr/bin/env ruby
+#This gets you the correct answer, but here's a look at cleaning it up.
 
-def is_palindrome(n)
-  n == n.to_s.reverse.to_i
-end
+##    THis comparisson for the palindrome is great, but it doesn't have to be done in a seperate method, 
+##    I Did tweek it a little bit, but not much, honestly probably wouldn't make a difference       
+##                        => (n).to_s == (n).to_s.reverse <=
+#   def is_palindrome(n)
+#     n == n.to_s.reverse.to_i
+#   end
 
-def solve
-  palindromes = []
-  1.upto(999) do |i|
-    1.upto(999) do |j|
-      candidate = i * j
-      if is_palindrome(candidate)
-        palindromes << candidate
-      end   
-    end
-  end  
-  palindromes.max
-end
+##    Here I turned it from this   ##
+# def solve
+#   palindromes = []
+#   1.upto(999) do |i|
+#     1.upto(999) do |j|
+#       candidate = i * j
+#       if is_palindrome(candidate)
+#         palindromes << candidate
+#       end   
+#     end
+#   end  
+#   palindromes.max
+# end
 
-if __FILE__ == $0
-  puts solve
-end
+
+##to this this
+
+palindromes = []
+100.upto(999) { |x| 100.upto(999) { |y| palindromes << (x * y) if (x * y).to_s == (x * y).to_s.reverse } }
+puts palindromes.max
+
+##Just some code condensing.
+
